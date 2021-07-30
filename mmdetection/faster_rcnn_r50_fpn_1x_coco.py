105da9d43e79332d55e56737def8ff77f0880f1b
shekar-almug <52408211+shekar-almug@users.noreply.github.com>
2021-07-30 13:45:17 +0530

    Add files via upload

---
+++ mmdetection/faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '/content/drive/MyDrive/mmdetection/_base_/datasets/coco_detection.py',
+    '/content/drive/MyDrive/mmdetection/_base_/datasets/coco_detection.py',
+    '/content/drive/MyDrive/mmdetection/_base_/schedules/schedule_1x.py', '/content/drive/MyDrive/mmdetection/_base_/default_runtime.py'
+]
